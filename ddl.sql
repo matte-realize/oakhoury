526b79020e8594c423175f32edf36686e828c5cf
Jordan Pinnick <geometryse@gmail.com>
2025-03-25 10:12:26 -0700

    Format code

--- ddl.sql
+++ ddl.sql
@@ -1,32 +1,36 @@
 -- Creates tables in Postgres v.17.4
 
-CREATE TABLE neighborhoods (
-    name varchar(100) primary key
+CREATE TABLE neighborhoods
+(
+    name VARCHAR(100) PRIMARY KEY
 );
 
-CREATE TABLE residents (
-    id serial primary key,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(100) unique not null,
-    password varchar(50),
-    street varchar(50),
-    zip_code char(5),
-    is_volunteer boolean,
-    neighborhood varchar(100) references neighborhoods(name)
+CREATE TABLE residents
+(
+    id           SERIAL PRIMARY KEY,
+    first_name   VARCHAR(50),
+    last_name    VARCHAR(50),
+    email        VARCHAR(100) UNIQUE NOT NULL,
+    password     VARCHAR(50),
+    street       VARCHAR(50),
+    zip_code     CHAR(5),
+    is_volunteer BOOLEAN,
+    neighborhood VARCHAR(100) REFERENCES neighborhoods (name)
 );
 
-CREATE TABLE organization_members (
-    resident_id integer primary key references residents(id),
-    role varchar(50),
-    start_date date
+CREATE TABLE organization_members
+(
+    resident_id INTEGER PRIMARY KEY REFERENCES residents (id),
+    role        VARCHAR(50),
+    start_date  DATE
 );
 
-CREATE TABLE volunteer_applications (
-    resident_id integer primary key references residents(id),
-    created date,
-    approved boolean,
-    notes text
+CREATE TABLE volunteer_applications
+(
+    resident_id INTEGER PRIMARY KEY REFERENCES residents (id),
+    created     DATE,
+    approved    BOOLEAN,
+    notes       TEXT
 );
 
 CREATE TYPE tolerance AS ENUM ('moderate', 'high', 'very high');
@@ -34,99 +38,110 @@ CREATE TYPE rate AS ENUM ('slow', 'moderate', 'fast', 'very fast');
 CREATE TYPE foliage AS ENUM ('deciduous', 'drought-deciduous', 'evergreen', 'semi-evergreen', 'late-deciduous');
 CREATE TYPE root_damage AS ENUM ('low', 'moderate', 'high');
 CREATE TYPE nursery_availability AS ENUM ('low', 'moderate', 'high');
-CREATE TABLE trees (
-    id serial primary key,
-    common_name varchar(100) unique,
-    scientific_name varchar(100) unique,
-    height_range int4range,
-    width_range int4range,
-    minimum_planting_bed_width integer,
-    plantable_under_power_lines boolean,
-    native_to_ca boolean,
-    drought_tolerance tolerance,
-    growth_rate rate,
-    foliage_type foliage,
-    debris varchar(50),
-    root_damage_potential root_damage,
-    nursery_availability nursery_availability,
-    visual_attraction varchar(50)[],
-    pZHarshSites boolean,
-    pZBay boolean,
-    pZUrbanized boolean,
-    pZNearNaturalAreas boolean,
-    inventory integer check(inventory >= 0)
+CREATE TABLE trees
+(
+    id                          SERIAL PRIMARY KEY,
+    common_name                 VARCHAR(100) UNIQUE,
+    scientific_name             VARCHAR(100) UNIQUE,
+    height_range                int4range,
+    width_range                 int4range,
+    minimum_planting_bed_width  INTEGER,
+    plantable_under_power_lines BOOLEAN,
+    native_to_ca                BOOLEAN,
+    drought_tolerance           tolerance,
+    growth_rate                 rate,
+    foliage_type                foliage,
+    debris                      VARCHAR(50),
+    root_damage_potential       root_damage,
+    nursery_availability        nursery_availability,
+    visual_attraction           VARCHAR(50)[],
+    pzharshsites                BOOLEAN,
+    pzbay                       BOOLEAN,
+    pzurbanized                 BOOLEAN,
+    pznearnaturalareas          BOOLEAN,
+    inventory                   INTEGER CHECK (inventory >= 0)
 );
 
-CREATE TABLE tree_requests (
-    id serial primary key,
-    resident_id integer references residents(id),
-    submission_timestamp timestamp,
-    tree_id integer references trees(id),
-    site_description text,
-    approved boolean,
-    unique(resident_id, submission_timestamp)
+CREATE TABLE tree_requests
+(
+    id                   SERIAL PRIMARY KEY,
+    resident_id          INTEGER REFERENCES residents (id),
+    submission_timestamp TIMESTAMP,
+    tree_id              INTEGER REFERENCES trees (id),
+    site_description     TEXT,
+    approved             BOOLEAN,
+    UNIQUE (resident_id, submission_timestamp)
 );
 
 CREATE TYPE status AS ENUM ('pending', 'approved', 'denied');
-CREATE TABLE permits (
-    resident_id integer references residents(id),
-    tree_request_id integer references tree_requests(id),
-    status status,
-    decision_date date,
-    PRIMARY KEY(resident_id, tree_request_id)
+CREATE TABLE permits
+(
+    resident_id     INTEGER REFERENCES residents (id),
+    tree_request_id INTEGER REFERENCES tree_requests (id),
+    status          status,
+    decision_date   DATE,
+    PRIMARY KEY (resident_id, tree_request_id)
 );
 
 -- Abstract table to hold scheduled_events
-CREATE TABLE scheduled_events (
-    event_id serial primary key,
-    tree_request_id integer references tree_requests(id),
-    event_timestamp timestamp,
-    cancelled boolean,
-    notes text
+CREATE TABLE scheduled_events
+(
+    event_id        SERIAL PRIMARY KEY,
+    tree_request_id INTEGER REFERENCES tree_requests (id),
+    event_timestamp TIMESTAMP,
+    cancelled       BOOLEAN,
+    notes           TEXT
 );
 
-CREATE TABLE scheduled_plantings (
+CREATE TABLE scheduled_plantings
+(
     -- inherits all from scheduled_events
-    primary key (event_id)
-) inherits (scheduled_events);
+    PRIMARY KEY (event_id)
+) INHERITS (scheduled_events);
 
 
-CREATE TABLE scheduled_visits (
-    organization_member_id integer references organization_members(resident_id),
-    primary key (event_id)
-) inherits (scheduled_events);
+CREATE TABLE scheduled_visits
+(
+    organization_member_id INTEGER REFERENCES organization_members (resident_id),
+    PRIMARY KEY (event_id)
+) INHERITS (scheduled_events);
 
-CREATE TABLE visit_events (
-    scheduled_visit_id integer primary key references scheduled_visits(event_id),
-    observations text,
-    photo_library_link varchar(50),
-    additional_visit_required boolean
+CREATE TABLE visit_events
+(
+    scheduled_visit_id        INTEGER PRIMARY KEY REFERENCES scheduled_visits (event_id),
+    observations              TEXT,
+    photo_library_link        VARCHAR(50),
+    additional_visit_required BOOLEAN
 );
 
-CREATE TABLE planting_events (
-    scheduled_planting_id integer primary key references scheduled_plantings(event_id),
-    observations text,
-    before_photos_library_link varchar(100),
-    after_photos_library_link varchar(100),
-    successful boolean
+CREATE TABLE planting_events
+(
+    scheduled_planting_id      INTEGER PRIMARY KEY REFERENCES scheduled_plantings (event_id),
+    observations               TEXT,
+    before_photos_library_link VARCHAR(100),
+    after_photos_library_link  VARCHAR(100),
+    successful                 BOOLEAN
 );
 
 -- junction tables
 
-CREATE TABLE organization_members_lead_planting_events (
-    organization_member_id integer references organization_members(resident_id),
-    planting_event_id integer references planting_events(scheduled_planting_id),
-    primary key(organization_member_id, planting_event_id)
+CREATE TABLE organization_members_lead_planting_events
+(
+    organization_member_id INTEGER REFERENCES organization_members (resident_id),
+    planting_event_id      INTEGER REFERENCES planting_events (scheduled_planting_id),
+    PRIMARY KEY (organization_member_id, planting_event_id)
 );
 
-CREATE TABLE scheduled_plantings_have_volunteers (
-    planting_event_id integer references scheduled_plantings(event_id),
-    volunteer_id integer references residents(id),
-    primary key(planting_event_id, volunteer_id)
+CREATE TABLE scheduled_plantings_have_volunteers
+(
+    planting_event_id INTEGER REFERENCES scheduled_plantings (event_id),
+    volunteer_id      INTEGER REFERENCES residents (id),
+    PRIMARY KEY (planting_event_id, volunteer_id)
 );
 
-CREATE TABLE planting_events_have_volunteers (
-    planting_event_id integer references planting_events(scheduled_planting_id),
-    volunteer_id integer references residents(id),
-    primary key(planting_event_id,  volunteer_id)
+CREATE TABLE planting_events_have_volunteers
+(
+    planting_event_id INTEGER REFERENCES planting_events (scheduled_planting_id),
+    volunteer_id      INTEGER REFERENCES residents (id),
+    PRIMARY KEY (planting_event_id, volunteer_id)
 );
--- tasks.sql
+++ tasks.sql
@@ -1,105 +1,108 @@
 -- Registering a new user
 INSERT INTO residents (first_name, last_name, email, password, street, zip_code, is_volunteer, neighborhood)
-    VALUES (
-    :first_name,
-    :last_name,
-    :email,
-    :password,
-    :street,
-    :zipcode,
-    false,
-    :neighborhood
-);
+VALUES
+    (:first_name,
+     :last_name,
+     :email,
+     :password,
+     :street,
+     :zipcode,
+     FALSE,
+     :neighborhood);
 
 -- Confirming user login details are correct (login)
-SELECT * FROM residents r
-WHERE r.email = :email
+SELECT *
+FROM
+    residents r
+WHERE
+      r.email = :email
   AND r.password = :password;
 
 -- Accepting a request for a tree
 INSERT INTO tree_requests (resident_id, submission_timestamp, tree_id, site_description, approved)
-VALUES (
-        :resident_id,
-        current_timestamp,
-        :tree_id, -- find tree id
-        :site_description,
-        false
-       );
+VALUES
+    (:resident_id,
+     CURRENT_TIMESTAMP,
+     :tree_id, -- find tree id
+     :site_description,
+     FALSE);
 
 -- Approve a request for a tree
 UPDATE tree_requests
-SET approved = true
-WHERE id = :id;
+SET
+    approved = TRUE
+WHERE
+    id = :id;
 
 -- Schedule a visit
 INSERT INTO scheduled_visits (event_timestamp, cancelled, notes, organization_member_id)
-VALUES (
-        :timestamp,
-        false,
-        :notes,
-        :organization_member_id
-       );
+VALUES
+    (:timestamp,
+     FALSE,
+     :notes,
+     :organization_member_id);
 
 
 -- Cancel a visit
 UPDATE scheduled_visits
-SET cancelled = true
-WHERE event_id = :event_id;
+SET
+    cancelled = TRUE
+WHERE
+    event_id = :event_id;
 
 -- Record info gathered from a visit
 INSERT INTO visit_events (scheduled_visit_id, observations, photo_library_link, additional_visit_required)
-VALUES (
-        :scheduled_visit_id,
-        :observations
-        :photo_library_link,
-        :additional_visit_required
-       );
+VALUES
+    (:scheduled_visit_id,
+     :observations
+     :photo_library_link,
+     :additional_visit_required);
 
 -- Schedule a planting (without any org members or volunteers yet)
 INSERT INTO scheduled_plantings (event_timestamp, cancelled, notes)
-VALUES (
-        :timestamp,
-        false,
-        :notes
-       );
+VALUES
+    (:timestamp,
+     FALSE,
+     :notes);
 
 -- Add an organization member to a scheduled planting
 INSERT INTO organization_members_lead_planting_events (organization_member_id, planting_event_id)
-VALUES (
-        :organization_member_id,
-        :planting_event_id
-       );
+VALUES
+    (:organization_member_id,
+     :planting_event_id);
 
 -- Add a volunteer to a scheduled planting
 INSERT INTO scheduled_plantings_have_volunteers (volunteer_id, planting_event_id)
-VALUES (
-        :volunteer_id,
-        :planting_event_id
-       );
+VALUES
+    (:volunteer_id,
+     :planting_event_id);
 
 -- Record info after a scheduled planting
-INSERT INTO planting_events (scheduled_planting_id, observations, before_photos_library_link, after_photos_library_link, successful)
-VALUES (
-        :scheduled_planting_id,
-        :observations,
-        :before_photos_library_link,
-        :after_photos_library_link,
-        :successful
-       );
+INSERT INTO planting_events (scheduled_planting_id, observations, before_photos_library_link, after_photos_library_link,
+                             successful)
+VALUES
+    (:scheduled_planting_id,
+     :observations,
+     :before_photos_library_link,
+     :after_photos_library_link,
+     :successful);
 
 -- Decrement the inventory for that tree planted
 UPDATE trees
-SET inventory = inventory - 1
-WHERE id = :id;
+SET
+    inventory = inventory - 1
+WHERE
+    id = :id;
 
 -- Add a volunteer that actually participated
 INSERT INTO planting_events_have_volunteers (planting_event_id, volunteer_id)
-VALUES (
-        :planting_event_id,
-        :volunteer_id
-       );
+VALUES
+    (:planting_event_id,
+     :volunteer_id);
 
 -- Update the inventory for a tree
 UPDATE trees
-SET inventory = :inventory
-WHERE id = :id;
+SET
+    inventory = :inventory
+WHERE
+    id = :id;
