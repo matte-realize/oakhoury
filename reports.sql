ebcf9251e6be801ad73e2bac60c58db4e3b928d5
Devan Kumar <devank978@gmail.com>
2025-03-25 13:10:43 -0700

    Reformatted first report query and added justification

--- reports.sql
+++ reports.sql
@@ -3,30 +3,43 @@
 -- The following link taught me about the LIMIT keyword
 -- https://razorsql.com/articles/postgresql_select_top_syntax.html#:~:text=Postgres%20does%20have%20a%20way,limit%20keyword%20must%20be%20used.&text=PostgreSQL%20also%20gives%20the%20ability,limit%20N%20offset%20Y%20syntax.
 
-SELECT r.first_name || ' ' || r.last_name AS volunteer_name,
-       MIN(sp.event_timestamp) AS first_planting,
-       MAX(sp.event_timestamp) AS most_recent_planting,
-       COUNT(pev) AS trees_planted,
-       (SELECT DATE_PART('Year', sp2.event_timestamp)
-        FROM planting_events_have_volunteers AS pev2
-                 INNER JOIN planting_events AS p2 ON pev2.planting_event_id = p2.scheduled_planting_id
-                 INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
-        WHERE pev2.volunteer_id = r.id
-        GROUP BY DATE_PART('Year', sp2.event_timestamp)
-        ORDER BY COUNT(*) DESC,
-        LIMIT 1) AS peak_year,
-       (SELECT COUNT(*)
-        FROM planting_events_have_volunteers pev2
-                 INNER JOIN planting_events AS p2 ON pev2.planting_event_id = p2.scheduled_planting_id
-                 INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
-        WHERE pev2.volunteer_id = r.id
-        GROUP BY DATE_PART('Year', sp2.event_timestamp)
-        ORDER BY COUNT(*) DESC
-        LIMIT 1) AS trees_planted_in_peak_year
-FROM residents as r
-    INNER JOIN planting_events_have_volunteers AS pev ON r.id = pev.volunteer_id
-    INNER JOIN planting_events AS pe ON pev.planting_event_id = pe.scheduled_planting_id
-    INNER JOIN scheduled_plantings AS sp ON pe.scheduled_planting_id = sp.event_id
-WHERE r.is_volunteer = true AND pe.successful = true
+-- The following query gets us a report about the most active volunteers, this is important because it helps the organization
+-- keep track of the impact that each volunteer has helped cause.
+
+SELECT
+    r.first_name || ' ' || r.last_name AS volunteer_name,
+    MIN(sp.event_timestamp)            AS first_planting,
+    MAX(sp.event_timestamp)            AS most_recent_planting,
+    COUNT(pev)                         AS trees_planted,
+    (SELECT
+         DATE_PART('Year', sp2.event_timestamp)
+     FROM
+         planting_events_have_volunteers AS pev2
+             INNER JOIN planting_events AS p2 ON pev2.planting_event_id = p2.scheduled_planting_id
+             INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
+     WHERE
+         pev2.volunteer_id = r.id
+     GROUP BY DATE_PART('Year', sp2.event_timestamp)
+     ORDER BY COUNT(*) DESC
+     LIMIT 1)                          AS peak_year,
+    (SELECT
+         COUNT(*)
+     FROM
+         planting_events_have_volunteers pev2
+             INNER JOIN planting_events AS p2 ON pev2.planting_event_id = p2.scheduled_planting_id
+             INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
+     WHERE
+         pev2.volunteer_id = r.id
+     GROUP BY DATE_PART('Year', sp2.event_timestamp)
+     ORDER BY COUNT(*) DESC
+     LIMIT 1)                          AS trees_planted_in_peak_year
+FROM
+    residents AS r
+        INNER JOIN planting_events_have_volunteers AS pev ON r.id = pev.volunteer_id
+        INNER JOIN planting_events AS pe ON pev.planting_event_id = pe.scheduled_planting_id
+        INNER JOIN scheduled_plantings AS sp ON pe.scheduled_planting_id = sp.event_id
+WHERE
+      r.is_volunteer = TRUE
+  AND pe.successful = TRUE
 GROUP BY r.first_name, r.last_name, r.id
 ORDER BY trees_planted_in_peak_year DESC, trees_planted DESC;
