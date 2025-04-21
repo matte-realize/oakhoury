7c41d96e325782ae24a78c7a603bbecd9bdf718d
Devan Kumar <devank978@gmail.com>
2025-04-21 11:19:43 -0700

    Parameterized report 1

--- reports.sql
+++ reports.sql
@@ -4,22 +4,25 @@
 -- https://razorsql.com/articles/postgresql_select_top_syntax.html#:~:text=Postgres%20does%20have%20a%20way,limit%20keyword%20must%20be%20used.&text=PostgreSQL%20also%20gives%20the%20ability,limit%20N%20offset%20Y%20syntax.
 
 -- The following query gets us a report about the most active volunteers, this is important because it helps the organization
--- keep track of the impact that each volunteer has helped cause.
+-- keep track of the impact that each volunteer has helped cause. Year has been parameterized so that the user can query
+-- data for whichever year they desire, this parameterization allows for the organization to track the impact and activity
+-- of each volunteer more clearly.
 
 SELECT
     r.first_name || ' ' || r.last_name AS volunteer_name,
     MIN(sp.event_timestamp)            AS first_planting,
     MAX(sp.event_timestamp)            AS most_recent_planting,
-    COUNT(pev)                         AS trees_planted,
+    COUNT(pev)                         AS trees_planted_this_year,
     (SELECT
-         DATE_PART('Year', sp2.event_timestamp)
+         EXTRACT(YEAR FROM sp2.event_timestamp::TIMESTAMP)
      FROM
          planting_events_have_volunteers AS pev2
              INNER JOIN planting_events AS p2 ON pev2.planting_event_id = p2.scheduled_planting_id
              INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
      WHERE
          pev2.volunteer_id = r.id
-     GROUP BY DATE_PART('Year', sp2.event_timestamp)
+     GROUP BY
+         EXTRACT(YEAR FROM sp2.event_timestamp::TIMESTAMP)
      ORDER BY COUNT(*) DESC
      LIMIT 1)                          AS peak_year,
     (SELECT
@@ -30,7 +33,8 @@ SELECT
              INNER JOIN scheduled_plantings AS sp2 ON p2.scheduled_planting_id = sp2.event_id
      WHERE
          pev2.volunteer_id = r.id
-     GROUP BY DATE_PART('Year', sp2.event_timestamp)
+     GROUP BY
+         EXTRACT(YEAR FROM sp2.event_timestamp::TIMESTAMP)
      ORDER BY COUNT(*) DESC
      LIMIT 1)                          AS trees_planted_in_peak_year
 FROM
@@ -41,8 +45,9 @@ FROM
 WHERE
       r.is_volunteer = TRUE
   AND pe.successful = TRUE
+  AND EXTRACT(YEAR FROM sp.event_timestamp::TIMESTAMP) = :p_year
 GROUP BY r.first_name, r.last_name, r.id
-ORDER BY trees_planted_in_peak_year DESC, trees_planted DESC;
+ORDER BY trees_planted_this_year DESC, trees_planted_in_peak_year DESC;
 
 -- The following query gives us a report regarding the organization members that lead plantings and attend visits; it
 -- displays the amount of plantings they've led, visits they've attended, their peak years and activity for both plantings
